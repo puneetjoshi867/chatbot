ccb6b9ee6a62428661ca88bc11357d565d201695
puneetjoshi867 <puneetjoshi867@gmail.com>
2026-05-13 13:30:43 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+#creating files 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.pynb
+touch .evn
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+
+echo "successfully created the template for your project"
